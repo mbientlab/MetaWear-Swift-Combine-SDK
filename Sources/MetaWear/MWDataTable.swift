5bb19afb2dfdccfccb57ebc7b32d246025fbeb51
Ryan Ferrell <importRyan@gmail.com>
2022-01-05 22:39:03 -0800

    Add additional date columns to MWDataTable

--- Sources/MetaWear/MWDataTable.swift
+++ Sources/MetaWear/MWDataTable.swift
@@ -23,30 +23,129 @@ public struct MWDataTable {
         row.joined(separator: delimiter).appending("\n")
     }
 
-    public init(download: MWData.LogDownload, startDate: Date) {
+    /// - Parameter startDate: Time that the logging session started. Used to calculate elapsed time in an output CSV files, potentially synchronized across multiple devices. (A MetaWear ticks time, but lacks a calendar-aware clock.)
+    ///
+    public init(
+        download: MWData.LogDownload,
+        startDate: Date,
+        dateColumns: [ExtraDateColumns] = ExtraDateColumns.allCases
+    ) {
         self.source = download.logger
         let utilities = download.logger.downloadUtilities
-        self.headerRow = utilities.columnHeadings
+        self.headerRow = dateColumns.updatingHeader(utilities.columnHeadings)
+
+        guard dateColumns.isEmpty == false else {
+            self.rows = utilities.convertRawDataToCSVColumns(download.data)
+            return
+        }
+
         self.rows = utilities.convertRawDataToCSVColumns(download.data)
+        .compactMap { row -> [String]? in
+            guard let first = row.first, let interval = Double(first) else { return nil }
+            let date = Date(timeIntervalSince1970: interval)
+            let extras = dateColumns.makeDataColumns(for: date, startDate: startDate)
+            var columns = row
+            columns.insert(contentsOf: extras, at: 1)
+            return columns
+        }
     }
 
-    public init<S: MWStreamable>(streamed: [(time: Date, value: S.DataType)],
-                                 _ streamable: S,
-                                 startDate: Date
+    /// - Parameter startDate: Time that the streaming session started. Used to calculate elapsed time in an output CSV files, potentially synchronized across multiple devices. (A MetaWear ticks time, but lacks a calendar-aware clock.)
+    ///
+    public init<S: MWStreamable>(
+        streamed: [(time: Date, value: S.DataType)],
+        _ streamable: S,
+        startDate: Date,
+        dateColumns: [ExtraDateColumns] = ExtraDateColumns.allCases
     ) {
         self.source = streamable.signalName
-        let utilties = source.downloadUtilities
-        self.headerRow = utilties.columnHeadings
-        self.rows = streamed.map(streamable.asColumns)
+        self.headerRow = dateColumns.updatingHeader(source.downloadUtilities.columnHeadings)
+        self.rows = streamed.map {
+            var columns = streamable.asColumns($0)
+            if dateColumns.isEmpty { return columns }
+            let extras = dateColumns.makeDataColumns(for: $0.time, startDate: startDate)
+            columns.insert(contentsOf: extras, at: 1)
+            return columns
+        }
+    }
+    
+    /// - Parameter startDate: Time that the streaming session started. Used to calculate elapsed time in an output CSV files, potentially synchronized across multiple devices. (A MetaWear ticks time, but lacks a calendar-aware clock.)
+    ///
+    public init<P: MWPollable>(
+        streamed: [(time: Date, value: P.DataType)],
+        _ streamable: P,
+        startDate: Date,
+        dateColumns: [ExtraDateColumns] = ExtraDateColumns.allCases
+    ) {
+        self.source = streamable.signalName
+        self.headerRow = dateColumns.updatingHeader(source.downloadUtilities.columnHeadings)
+
+        self.rows = streamed.map {
+            var columns = streamable.asColumns($0)
+            if dateColumns.isEmpty { return columns }
+            let extras = dateColumns.makeDataColumns(for: $0.time, startDate: startDate)
+            columns.insert(contentsOf: extras, at: 1)
+            return columns
+        }
+    }
+}
+
+// MARK: - Timestamp and Elapsed Time Columns
+
+public extension MWDataTable {
+
+    enum ExtraDateColumns: Int, Equatable, Hashable, CaseIterable {
+        case date
+        case elapsed
+
+        var header: String {
+            switch self {
+                case .date:
+                    let zone = Self.zoneFormatter.string(from: Date())
+                    return "Timestamp (\(zone))"
+                case .elapsed: return "Elapsed (s)"
+            }
+        }
+
+        public func string(date: Date, startdate: Date) -> String {
+            switch self {
+                case .date:
+                    return Self.formatter.string(from: date)
+                case .elapsed:
+                    let elapsed = startdate.distance(to: date)
+                    return String(format: "%.\(Self.timeElapsedDecimals)f", elapsed)
+            }
+        }
+
+        /// Max is nanoseconds
+        public static var timeElapsedDecimals = 3
+
+        public static var zoneFormatter: DateFormatter = {
+            let f = DateFormatter()
+            f.dateFormat = "Z"
+            return f
+        }()
+
+        public static var formatter: DateFormatter = {
+            let f = DateFormatter()
+            f.dateFormat = "yyyy-MM-dd'T'HH.mm.ss.SSS"
+            return f
+        }()
+    }
+}
+
+internal extension Array where Element == MWDataTable.ExtraDateColumns {
+
+    func updatingHeader(_ headerColumns: [String]) -> [String] {
+        if isEmpty { return headerColumns }
+        var update = headerColumns
+        update.insert(contentsOf: self.map(\.header), at: 1)
+        return update
     }
 
-    public init<P: MWPollable>(streamed: [(time: Date, value: P.DataType)],
-                               _ streamable: P,
-                               startDate: Date
-    ) {
-        self.source = streamable.signalName
-        let utilties = source.downloadUtilities
-        self.headerRow = utilties.columnHeadings
-        self.rows = streamed.map(streamable.asColumns)
+    func makeDataColumns(for date: Date, startDate: Date) -> [String] {
+        map { column in
+            column.string(date: date, startdate: startDate)
+        }
     }
 }
--- Tests/MetaWearTests/MWDataTableTests.swift
+++ Tests/MetaWearTests/MWDataTableTests.swift
@@ -8,26 +8,83 @@ import CoreBluetooth
 
 class MWDataTableTests: XCTestCase {
 
-    func test_MWDataConvertible_CreatesCSVStrings() {
+    func test_MWDataConvertible_CreatesCSVStrings_EpochOnly() {
         let exp = """
 Epoch,X,Y,Z
-1639715268.2441,-0.0450,0.0048,1.0206
-1639715268.2441,-0.0455,0.0063,1.0309
-1639715268.2441,-0.0468,0.0046,1.0305
-1639715268.2441,-0.0440,0.0046,1.0290
-1639715268.2441,-0.0448,0.0065,1.0294
+1639715268.244,-0.0450,0.0048,1.0206
+1639715268.245,-0.0455,0.0063,1.0309
+1639715268.246,-0.0468,0.0046,1.0305
+1639715268.247,-0.0440,0.0046,1.0290
+1639715268.248,-0.0448,0.0065,1.0294
 """
         let date = Date(timeIntervalSince1970: 1639715268.2441)
         let streamableData: [(time: Date, value: MWAccelerometer.DataType)] = [
-            (date, SIMD3<Float>(-0.04498291, 0.004760742, 1.0205688)),
-            (date, SIMD3<Float>(-0.04547119, 0.0063476562, 1.0308838)),
-            (date, SIMD3<Float>(-0.04675293, 0.0045776367, 1.0304565)),
-            (date, SIMD3<Float>(-0.044006348, 0.0045776367, 1.0289917)),
-            (date, SIMD3<Float>(-0.044799805, 0.0065307617, 1.029419)),
+            (date.addingTimeInterval(0.000), SIMD3<Float>(-0.04498291, 0.004760742, 1.0205688)),
+            (date.addingTimeInterval(0.001), SIMD3<Float>(-0.04547119, 0.0063476562, 1.0308838)),
+            (date.addingTimeInterval(0.002), SIMD3<Float>(-0.04675293, 0.0045776367, 1.0304565)),
+            (date.addingTimeInterval(0.003), SIMD3<Float>(-0.044006348, 0.0045776367, 1.0289917)),
+            (date.addingTimeInterval(0.004), SIMD3<Float>(-0.044799805, 0.0065307617, 1.029419)),
         ]
         let sut = MWDataTable(streamed: streamableData,
                               .accelerometer(rate: .hz100, gravity: .g16),
-                              startDate: date
+                              startDate: date,
+                              dateColumns: []
+        )
+        let result = sut.makeCSV()
+
+        XCTAssertEqual(exp.count, result.count)
+        XCTAssertEqual(exp, result)
+    }
+
+    func test_MWDataConvertible_CreatesCSVStrings_EpochElapsed() {
+        let exp = """
+Epoch,Elapsed (s),X,Y,Z
+1639715268.244,0.000,-0.0450,0.0048,1.0206
+1639715268.245,0.001,-0.0455,0.0063,1.0309
+1639715268.246,0.002,-0.0468,0.0046,1.0305
+1639715268.247,0.003,-0.0440,0.0046,1.0290
+1639715268.248,0.004,-0.0448,0.0065,1.0294
+"""
+        let date = Date(timeIntervalSince1970: 1639715268.2441)
+        let streamableData: [(time: Date, value: MWAccelerometer.DataType)] = [
+            (date.addingTimeInterval(0.000), SIMD3<Float>(-0.04498291, 0.004760742, 1.0205688)),
+            (date.addingTimeInterval(0.001), SIMD3<Float>(-0.04547119, 0.0063476562, 1.0308838)),
+            (date.addingTimeInterval(0.002), SIMD3<Float>(-0.04675293, 0.0045776367, 1.0304565)),
+            (date.addingTimeInterval(0.003), SIMD3<Float>(-0.044006348, 0.0045776367, 1.0289917)),
+            (date.addingTimeInterval(0.004), SIMD3<Float>(-0.044799805, 0.0065307617, 1.029419)),
+        ]
+        let sut = MWDataTable(streamed: streamableData,
+                              .accelerometer(rate: .hz100, gravity: .g16),
+                              startDate: date,
+                              dateColumns: [.elapsed]
+        )
+        let result = sut.makeCSV()
+
+        XCTAssertEqual(exp.count, result.count)
+        XCTAssertEqual(exp, result)
+    }
+
+    func test_MWDataConvertible_CreatesCSVStrings_EpochTimestampElapsed() {
+        let exp = """
+Epoch,Timestamp (-0800),Elapsed (s),X,Y,Z
+1639715268.244,2021-12-16T20.27.48.244,0.000,-0.0450,0.0048,1.0206
+1639715268.245,2021-12-16T20.27.48.245,0.001,-0.0455,0.0063,1.0309
+1639715268.246,2021-12-16T20.27.48.246,0.002,-0.0468,0.0046,1.0305
+1639715268.247,2021-12-16T20.27.48.247,0.003,-0.0440,0.0046,1.0290
+1639715268.248,2021-12-16T20.27.48.248,0.004,-0.0448,0.0065,1.0294
+"""
+        let date = Date(timeIntervalSince1970: 1639715268.2441)
+        let streamableData: [(time: Date, value: MWAccelerometer.DataType)] = [
+            (date.addingTimeInterval(0.000), SIMD3<Float>(-0.04498291, 0.004760742, 1.0205688)),
+            (date.addingTimeInterval(0.001), SIMD3<Float>(-0.04547119, 0.0063476562, 1.0308838)),
+            (date.addingTimeInterval(0.002), SIMD3<Float>(-0.04675293, 0.0045776367, 1.0304565)),
+            (date.addingTimeInterval(0.003), SIMD3<Float>(-0.044006348, 0.0045776367, 1.0289917)),
+            (date.addingTimeInterval(0.004), SIMD3<Float>(-0.044799805, 0.0065307617, 1.029419)),
+        ]
+        let sut = MWDataTable(streamed: streamableData,
+                              .accelerometer(rate: .hz100, gravity: .g16),
+                              startDate: date,
+                              dateColumns: MWDataTable.ExtraDateColumns.allCases
         )
         let result = sut.makeCSV()
 
