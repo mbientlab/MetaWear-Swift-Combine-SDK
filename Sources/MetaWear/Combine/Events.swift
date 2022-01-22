2814fcc1ccd222df4c0acedffb38eb485fe85a18
Ryan Ferrell <importRyan@gmail.com>
2022-01-22 12:36:10 -0800

    Draft button

---
+++ Sources/MetaWear/Combine/Events.swift
@@ -0,0 +1,61 @@
+// Copyright 2021 MbientLab Inc. All rights reserved. See LICENSE.MD.
+
+import Foundation
+import Combine
+import MetaWearCpp
+
+// See Timer for timed events.
+
+public extension Publisher where Output == MetaWear {
+
+    func recordEventsOnButtonDown(_ events: @escaping (MetaWear) -> MWPublisher<MetaWear>) -> MWPublisher<MetaWear> {
+        let upstream = mapToMWError().share()
+        let source = MWMechanicalButton()
+        let signal = upstream.map(\.board).flatMap(source.getDownEventSignal(board:))
+        return Publishers.Zip(upstream, signal)
+            .mapToMWError()
+            .flatMap { metawear, signal in
+                events(metawear)
+                    .flatMap { _ in signal.eventEndRecording() }
+                    .map { _ in metawear }
+                    .eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+    }
+}
+
+public extension Publisher where Output == (MetaWear, MWDataProcessorSignal) {
+
+    func recordEventsForSignal(
+    _ events @escaping (MetaWear) -> MWPublisher<MetaWear>
+    ) -> MWPublisher<MetaWear> {
+
+        self.mapToMWError().flatMap { mw, event in
+            mbl_mw_event_record_commands(event)
+            return
+        }
+    }
+}
+
+public extension MWBoard {
+
+    /// When pointing to an event signal, ends recording of an `MblMwEvent`. Combine wrapper for `mbl_mw_event_end_record`.
+    ///
+    func eventEndRecording() -> PassthroughSubject<Void,MWError> {
+
+        let subject = PassthroughSubject<Void,MWError>()
+        mbl_mw_event_end_record(self, bridge(obj: subject)) { (context, event, status) in
+            let _subject: PassthroughSubject<Void,MWError> = bridge(ptr: context!)
+
+            guard status == MWStatusCode.ok.cppValue else {
+                let code = MWStatusCode(cpp: status)?.rawValue ?? "Unknown code"
+                let msg = "Event end record failed: \(code)"
+                _subject.send(completion: .failure(.operationFailed(msg)))
+                return
+            }
+            _subject.send()
+            _subject.send(completion: .finished)
+        }
+        return subject
+    }
+}
